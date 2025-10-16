52ae0a2d5aa2e1d3e19be1524fd4f9db1724cbb7
catherine-personal <an.jiyeong5@gmail.com>
2025-10-16 19:39:31 +0100

    Added the footer.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 import SearchInput from "./search";
 
 export default function Home() {
@@ -8,6 +10,18 @@ export default function Home() {
         English Dictionary
       </h1>
       <SearchInput />
+      <div className="text-white p-8">
+        This code is developed by Catherine An and open-sourced on{" "}
+        <Link
+          href="https://github.com/catherine-j-an/dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-secondary transition"
+        >
+          GitHub
+        </Link>
+        .
+      </div>
     </div>
   );
 }
